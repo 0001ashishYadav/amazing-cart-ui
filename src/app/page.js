a8d11bd582a70031733c9d15cb37acaf1e7b47e4
000ashishYadav <000ashishyadav2003@gmail.com>
2025-04-11 11:37:41 +0530

    some work done in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,36 @@
 "use client";
+
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
-  return <p>Home Page</p>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          "https://api.store.inflection.org.in/products/public?limit=20"
+        );
+
+        const data = await res.json();
+
+        console.log(data);
+
+        setProducts(data);
+      } catch (error) {
+        console.log(err);
+      }
+    })();
+  }, []);
+  return (
+    <div className="grid md:grid-cols-6 gap-3 p-5">
+      {products.map((product) => (
+        <div key={product.product_code} className="border shadow rounded p-2">
+          <img src={product.thumbnail} />
+          <h4>{product.product_code}</h4>
+          <h3 className="font-medium">{product.product_name}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
