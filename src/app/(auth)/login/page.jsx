f251c2a383fa3515a71ab07bcdfe175fdabadcff
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-15 15:02:19 +0530

    make some changes in login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -60,12 +60,12 @@ function LoginPage() {
             {/* <!-- Username Input --> */}
             <div className="mb-4">
               <label for="username" className="block text-gray-600">
-                Username
+                Email
               </label>
               <input
-                type="text"
-                id="username"
-                name="username"
+                type="email"
+                id="email"
+                name="email"
                 required
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
