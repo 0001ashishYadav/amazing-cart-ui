2d615eaef5f57a90b592c36ac8a76a1fdc892521
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-16 12:02:01 +0530

    create apiclient function for api call

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,38 +1,56 @@
-// user authentication ...............................
-
-export const login = async (payload) => {
-  return await fetch(`${baseURL}/users/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-};
-
-export const signup = async (payload) => {
-  return await fetch(`${baseURL}/users/signup`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-};
-
-export const resetPassword = async (payload, token) => {
-  return await fetch(`${baseURL}/users/reset_password/${token}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
-};
-
-// product fetching ....................................
+import { getCookie } from "./cookies";
 
 export const baseURL = "https://api.store.inflection.org.in";
+
+export const apiClient = async (url, method, body = null, tokenName = "") => {
+  const accessToken = getCookie("access_token");
+
+  const defaultHeaders = {
+    "Content-Type": "application/json",
+  };
+
+  if (tokenName) {
+    defaultHeaders["Authorization"] = `Bearer ${getCookie(tokenName)}`;
+  } else {
+    defaultHeaders["Authorization"] = `Bearer ${accessToken}`;
+  }
+
+  let config;
+
+  if (!body || body === null) {
+    config = {
+      method: method,
+      headers: {
+        ...defaultHeaders,
+      },
+    };
+  } else {
+    config = {
+      method: method,
+      headers: {
+        ...defaultHeaders,
+      },
+      body: JSON.stringify(body),
+    };
+  }
+
+  const response = await fetch(`${baseURL}${url}`, config);
+  return await response.json();
+};
+
+// user authentication ...............................
+// Auth APIs
+apiClient.login = (payload) => apiClient("/users/login", "POST", payload);
+apiClient.signup = (payload) => apiClient("/users/signup", "POST", payload);
+apiClient.resetPassword = (token, payload) =>
+  apiClient(`/users/reset_password/${token}`, "PATCH", payload);
+apiClient.forgotPassword = (payload) =>
+  apiClient("/users/forgot_password", "POST", payload);
+
+apiClient.refreshAccessToken = () =>
+  apiClient("/users/renew_token", "GET", null, "refresh_token");
+
+// product fetching ....................................
 
 export const getProductDetails = async (slug) => {
   return await fetch(`${baseURL}/products/public/s/${slug}`, {
@@ -42,3 +60,15 @@ export const getProductDetails = async (slug) => {
     },
   });
 };
+
+// Category APIs
+apiClient.fetchCategories = () => apiClient("/categories/list", "GET");
+
+apiClient.getCategoryBySlug = (slug) =>
+  apiClient(`/categories/s/${slug}`, "GET");
+
+apiClient.getCategoryProductsBySlug = (slug) =>
+  apiClient(`/products/public/cat/${slug}`, "GET");
+
+apiClient.getAttributesByCategory = (id) =>
+  apiClient(`/attributes/${id}`, "GET");
