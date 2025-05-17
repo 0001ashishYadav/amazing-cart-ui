8713708a5cae917097d963f4f66b06f7e82464f5
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-17 14:05:38 +0530

    make some changes in card components

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -2,18 +2,17 @@ import Link from "next/link";
 import React from "react";
 
 const ProductCard = ({ product }) => {
+  {
+    console.log(product);
+  }
   return (
-    <div className="grid md:grid-cols-4 gap-3 p-5">
-      {product.map((product) => (
-        <div key={product.product_code} className="border shadow rounded p-2">
-          <img src={product.thumbnail} />
-          <h4>{product.product_code}</h4>
-          <p>{`Rs ${product.price}`}</p>
-          <Link href={`/products/${product.slug}`} className="font-medium">
-            {product.product_name}
-          </Link>
-        </div>
-      ))}
+    <div className="border shadow rounded p-2">
+      <img src={product.thumbnail} />
+      <h4>{product.product_code}</h4>
+      <p>{`Rs ${product.price}`}</p>
+      <Link href={`/products/${product.slug}`} className="font-medium">
+        {product.product_name}
+      </Link>
     </div>
   );
 };
