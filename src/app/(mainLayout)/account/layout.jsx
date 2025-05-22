609b96a8fd7d9a392b16b53eb5cc7950de2ced92
000ashishYadav <000ashishyadav2003@gmail.com>
2025-05-22 13:17:37 +0530

    make some changes in account layout page for protected routing

--- src/app/(mainLayout)/account/layout.jsx
+++ src/app/(mainLayout)/account/layout.jsx
@@ -1,7 +1,12 @@
+import ProtectedRoutes from "@/component/ProtectedRoutes";
 import React from "react";
 
 const AccountLayout = ({ children }) => {
-  return <div>AccountLayout</div>;
+  return (
+    <ProtectedRoutes>
+      <div>{children}</div>
+    </ProtectedRoutes>
+  );
 };
 
 export default AccountLayout;
